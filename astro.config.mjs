a44f05443d11c5483357bddd0ba64b2ffd7ed09d
printer <leonardotobe94@gmail.com>
2023-05-19 03:59:16 -0400

    Add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
+const url = import.meta.VERCEL_ENV === 'production' ? 'https://the-notes-ten.vercel.app/' : 'http://localhost:3001'
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://the-notes-ten.vercel.app/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
