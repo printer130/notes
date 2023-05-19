6d8d39e0fc6cb018c3147c48f76b78758c2e1609
printer <leonardotobe94@gmail.com>
2023-05-19 04:00:34 -0400

    Add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/static'
 
 import tailwind from "@astrojs/tailwind";
 
